abaae2a7481d58128ba69b50a7af5e2895305887
Dipam Sen <dipamdiptam@gmail.com>
2023-04-21 13:33:54 +0530

    strip out styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,8 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "./page.module.css";
 import Counter from "./components/Counter";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main>
       <div>Hello World</div>
       <Counter />
     </main>
